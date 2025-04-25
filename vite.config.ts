ba50cf27af600dcc57e1f689805db705e344dc7d
aziz.bensalem@pentabell.fr <aziz.bensalem@pentabell.fr>
2025-04-25 10:41:47 +0100

    remove base

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  // server: {
-  //   host: "::",
-  //   port: 8080,
-  // },
-  // plugins: [react(), mode === "development" && componentTagger()].filter(
-  //   Boolean
-  // ),
   plugins: [react()],
-  base: "/tunisian-wordle/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
